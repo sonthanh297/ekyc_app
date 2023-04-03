21628fbf5aeca73cb12ad1dd515fa14a7b7a8f8c
Phi Van Son <vansonk54@gmail.com>
2023-04-03 17:04:59 +0700

    cm

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { User } from '../model/user.model';
+import { AuthService } from '../service/auth.service';
+import { ValidationService } from '../service/validation.service';
+import { LoggerService } from '../service/logger.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit  {
+
+  loginForm: FormGroup = {} as FormGroup;
+
+  errorMessage: string = '';
+
+  get f(): { [key: string]: AbstractControl } {
+      return this.loginForm.controls;
+  }
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthService,
+              private logger: LoggerService) { }
+
+  ngOnInit() {
+      this.buildForm();
+  }
+
+  buildForm() {
+      this.loginForm = this.formBuilder.group({
+          email:      ['', [ Validators.required, ValidationService.emailValidator ]],
+          password:   ['', [ Validators.required, ValidationService.passwordValidator ]]
+      });
+  }
+
+  submit({ value, valid }: { value: User, valid: boolean }) {
+      this.authService.login(value)
+          .subscribe((status: boolean) => {
+              if (status) {
+                  if (this.authService.redirectUrl) {
+                      const redirectUrl = this.authService.redirectUrl;
+                      this.authService.redirectUrl = '';
+                      this.router.navigate([redirectUrl]);
+                  } else {
+                      this.router.navigate(['/customers']);
+                  }
+              } else {
+                  const loginError = 'Unable to login';
+                  this.errorMessage = loginError;
+
+              }
+          },
+          (err: any) => this.logger.log(err));
+  }
 }
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,33 +1,92 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, Output, EventEmitter, Inject, Directive } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { User } from '../model/user.model';
+import { UtilitiesService } from './utilities.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'https://api.example.com';
+//   private apiUrl = 'https://api.example.com';
 
-  constructor(private http: HttpClient) { }
+//   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string) {
-    return this.http.post(`${this.apiUrl}/login`, { email, password }).subscribe(
-      res => {
-        // Handle successful login
-      },
-      err => {
-        // Handle failed login
-      }
-    );
-  }
+//   login(email: string, password: string) {
+//     return this.http.post(`${this.apiUrl}/login`, { email, password }).subscribe(
+//       res => {
+//         // Handle successful login
+//       },
+//       err => {
+//         // Handle failed login
+//       }
+//     );
+//   }
 
-  logout() {
-    return this.http.post(`${this.apiUrl}/logout`, {}).subscribe(
-      res => {
-        // Handle successful logout
-      },
-      err => {
-        // Handle failed logout
-      });
+//   logout() {
+//     return this.http.post(`${this.apiUrl}/logout`, {}).subscribe(
+//       res => {
+//         // Handle successful logout
+//       },
+//       err => {
+//         // Handle failed logout
+//       });
+// }
+
+baseUrl = this.utilitiesService.getApiUrl();
+authUrl = this.baseUrl + '/api/auth';
+isAuthenticated = false;
+redirectUrl: string = '';
+@Output() authChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+constructor(private http: HttpClient, private utilitiesService: UtilitiesService) {  }
+
+private userAuthChanged(status: boolean) {
+   this.authChanged.emit(status); // Raise changed event
 }
+
+login(userLogin: User): Observable<boolean> {
+    return this.http.post<boolean>(this.authUrl + '/login', userLogin)
+        .pipe(
+            map(loggedIn => {
+                this.isAuthenticated = loggedIn;
+                this.userAuthChanged(loggedIn);
+                return loggedIn;
+            }),
+            catchError(this.handleError)
+        );
+}
+
+logout(): Observable<boolean> {
+    return this.http.post<boolean>(this.authUrl + '/logout', null)
+        .pipe(
+            map(loggedOut => {
+                this.isAuthenticated = !loggedOut;
+                this.userAuthChanged(!loggedOut); // Return loggedIn status
+                return loggedOut;
+            }),
+            catchError(this.handleError)
+        );
+}
+
+private handleError(error: HttpErrorResponse) {
+    console.error('server error:', error);
+    if (error.error instanceof Error) {
+      const errMessage = error.error.message;
+      return throwError(() => errMessage);
+      // return Observable.throw(err.text() || 'backend server error');
+    }
+    return throwError(() => error || 'Server error');
+}
+
+
+
+
+
+
+
+
 }
