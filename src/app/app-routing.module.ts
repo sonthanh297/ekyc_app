348d2831726d32bd1add35a390901e8d18806a5a
Phi Van Son <vansonk54@gmail.com>
2023-04-06 17:02:07 +0700

    cm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { TestComponent } from './test/test.component';
-import { UserModule } from './user/user.module';
 
-const authModule = () => import('./auth/auth.module').then(x => x.AuthModule);
 
 const routes: Routes = [
 
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AbstractControl, FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 
 import { User } from '../model/user.model';
 import { AuthService } from '../service/auth.service';
 import { ValidationService } from '../service/validation.service';
 import { LoggerService } from '../service/logger.service';
+import { AuthenticationService } from '../service/authentication.service';
+import { first } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-login',
@@ -15,5 +18,55 @@ import { LoggerService } from '../service/logger.service';
 
 export class LoginComponent  {
 
+  loginForm!: FormGroup;
+    loading = false;
+    submitted = false;
+    error = '';
 
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        // redirect to home if already logged in
+        if (this.authenticationService.userValue) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.error = '';
+        this.loading = true;
+        this.authenticationService.login(this.f['username'].value, this.f['password'].value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    // get return url from route parameters or default to '/'
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigate([returnUrl]);
+                },
+                error: error => {
+                    this.error = error;
+                    this.loading = false;
+                }
+            });
+    }
 }
